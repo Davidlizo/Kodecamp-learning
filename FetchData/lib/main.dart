6442aa0a37f9eaac6c9a1ad3862c3207f4e00006
David Nwabuzor <davidpainter421@gmail.com>
2024-06-08 20:08:35 +0100

    changes

--- FetchData/lib/main.dart
+++ FetchData/lib/main.dart
@@ -1,12 +1,27 @@
 import 'fetchdata.dart';
 
 void main() async {
-  final dataFetcher = DataFetcher<Map<String, dynamic>>();
+//for fetching different data types
+  final stringFetcher = DataFetcher<String>();
+  final listFetcher = DataFetcher<List<dynamic>>();
+  final intFetcher = DataFetcher<int>();
 
-  final sampleUrl = 'https://jsonplaceholder.typicode.com/posts/1';
+ // URLs for fetching data
+  const stringUrl = 'https://jsonplaceholder.typicode.com/posts/1';
+  const listUrl = 'https://jsonplaceholder.typicode.com/posts';
+  const intUrl = 'https://jsonplaceholder.typicode.com/posts/1';
   try {
-    final postData = await dataFetcher.fetchData(sampleUrl);
-    print('Post title: ${postData['title']}');
+    // fetch a string
+    final fetchedString = await stringFetcher.fetchData(stringUrl);
+    print('Fetched String: $fetchedString');
+
+    // fetch a list
+    final fetchedList = await listFetcher.fetchData(listUrl);
+    print('Fetched List: $fetchedList');
+
+    // fetch a int
+    final fetchedInt = await intFetcher.fetchData(intUrl);
+    print('Fetched Int: $fetchedInt');
   } catch (e) {
     print('Error: $e');
   }
